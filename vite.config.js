5d82ab23625635a8544e7834bb531f9599583866
Maazkhan100 <maazmkhan.123@gmail.com>
2025-11-24 14:28:55 +0500

    Add config js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // for username.github.io; use '/repo-name/' for project pages
+  base: "/maazkhan100.github.io/", // for username.github.io; use '/repo-name/' for project pages
 })
